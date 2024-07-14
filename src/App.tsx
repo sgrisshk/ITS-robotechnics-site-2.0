0703e19fcbd0ae179f742c9748373d19e7ce402d
Nikita <coma8765my@gmail.com>
2024-07-15 02:43:04 +0500

    feat(events): add events page (#1)
    * feat(events): add events page
    
    * fix(layout,colors): fix base colors and layout sizes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
     Route,
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import Menu from "./components/utils/menu/Menu";
 import {News} from "./components/pages/News/News";
 import {Events} from "./components/pages/Events/Events";
@@ -15,32 +15,35 @@ import {Partners} from "./components/pages/Partners/Partners";
 import {Contacts} from "./components/pages/Contacts/Contacts";
 import {Home} from "./components/pages/Home/Home";
 import {Hardathon} from "./components/pages/Hardathon/Hardathon";
-import {PartnersHardathon} from "./components/pages/Hardathon/Partners/PartnersHard";
 import {ProjectDetails} from "./components/pages/Hardathon/ProjectDetails/ProjectDetails";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+const queryClient = new QueryClient()
 
 function App() {
     return (
-    <Router>
-        <Helmet>
-            <title>Центр молодежной робототехники</title>
-        </Helmet>
-        <Menu />
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/news" element={<News/>} />
-                <Route path="/events" element={<Events/>} />
-                <Route path="/events/:id" element={<Event/>} />
-                <Route path="/achievements" element={<Achievements/>} />
-                <Route path="/partners" element={<Partners/>} />
-                <Route path="/contacts" element={<Contacts/>} />
-                <Route path="/hardathons" element={<Hardathon/>} />
-                {/*<Route path="/hardathons/:id/partners" element={<PartnersHardathon/>} />*/}
-                <Route path="/hardathons/:id/details" element={<ProjectDetails/>} />
-                
-            </Routes>
-    </Router>
-  );
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Helmet>
+                    <title>Центр молодежной робототехники</title>
+                </Helmet>
+                <Menu/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/events" element={<Events/>}/>
+                    <Route path="/events/:id" element={<Event/>}/>
+                    <Route path="/achievements" element={<Achievements/>}/>
+                    <Route path="/partners" element={<Partners/>}/>
+                    <Route path="/contacts" element={<Contacts/>}/>
+                    <Route path="/hardathons" element={<Hardathon/>}/>
+                    {/*<Route path="/hardathons/:id/partners" element={<PartnersHardathon/>} />*/}
+                    <Route path="/hardathons/:id/details" element={<ProjectDetails/>}/>
+
+                </Routes>
+            </Router>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
