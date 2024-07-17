5a4699fa8bc99105cfbfda67b95c5f0e2f64f5f2
Robocotik <alexander.starkin@gmail.com>
2024-07-17 14:15:48 +0300

    refactor(apis): group events and questeners to files

--- src/shared/apis/events.ts
+++ src/shared/apis/events.ts
@@ -10,4 +10,13 @@ const eventById: (id: number) => Promise<ShortEvent[]> = async (id: number) => {
     return events;
 };
 
-export { eventById };
+const eventList: () => Promise<ShortEvent[]> = async () => {
+    const res = await client.get('/api/v0/classic_events');
+    const events = res.data['events'];
+    if (res.status >= 300) {
+        throw new Error();
+    }
+    return events;
+};
+
+export { eventById, eventList };
